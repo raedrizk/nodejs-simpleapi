0d7285bdab183c3b5ff2df8ae20f476ad5f0e56c
Raed Soliman <raedrizk@gmail.com>
2025-05-15 13:46:24 -0400

    fixing port

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express ();
 const fs = require('fs');
 const path = require('path');
